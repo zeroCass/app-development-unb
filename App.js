ad0ffcc01adba4fe7e76fb4ea090268e4554bfd0
Mateus Valerio <mateusvalerio1@gmail.com>
2023-09-15 16:03:02 -0300

    config: initial config

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.titleText}>
+				Hello <Text style={styles.innerText}>World</Text>!
+			</Text>
+			<Text style={styles.baseText}>Some other text</Text>
+			<StatusBar style='auto' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	titleText: {
+		fontSize: 20,
+	},
+	innerText: {
+		fontWeight: 'bold',
+	},
+	baseText: {
+		fontSize: 16,
+	},
+})
