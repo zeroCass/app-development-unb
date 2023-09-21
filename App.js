106abd71cd1acf7a284f663864d04dd413a449a8
Thiago Tokarski <thiago.tokarski2001@gmail.com>
2023-09-20 21:02:34 -0300

    wip

--- App.js
+++ App.js
@@ -1,35 +1,5 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { RegisterError } from './src/screens/RegisterError/RegisterError'
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.titleText}>
-				Hello <Text style={styles.innerText}>World</Text>!
-			</Text>
-			<Text style={styles.titleText}>
-				Hello<Text style={styles.innerText}> World 2</Text>!
-			</Text>
-			<Text style={styles.baseText}>Some other text</Text>
-			<StatusBar style='auto' />
-		</View>
-	)
+	return <RegisterError />
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	titleText: {
-		fontSize: 20,
-	},
-	innerText: {
-		fontWeight: 'bold',
-	},
-	baseText: {
-		fontSize: 16,
-	},
-})
