404686951ca6fb12753e5b346a0035a3014f0ac1
Thiago Tokarski <thiago.tokarski2001@gmail.com>
2023-09-21 00:22:58 -0300

    fix: icon

--- src/components/TopSideMenu.js
+++ src/components/TopSideMenu.js
@@ -2,14 +2,11 @@ import { View, Text, Button, StyleSheet, StatusBar } from 'react-native'
 import PropTypes from 'prop-types'
 import { Ionicons } from '@expo/vector-icons'
 
-const TopSideMenu = ({ title }) => {
+const TopSideMenu = ({ title, icon }) => {
 	return (
 		<>
-			{/* <View styles={styles.statusBar}>
-				<StatusBar backgroundColor={'#88c9bf'} barStyle={'light-content'} translucent={false} />
-			</View> */}
 			<View style={styles.container}>
-				<Ionicons name='menu' size={28} color='#434343' style={styles.icon} />
+				<Ionicons name={icon} size={28} color='#434343' style={styles.icon} />
 				<Text style={styles.text}>{title}</Text>
 			</View>
 		</>
