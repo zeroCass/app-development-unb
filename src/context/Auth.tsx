8c133ea4a92fda11c7a737e419d28ad8fd856fe6
Mateus Valério <mateusvalerio1@gmail.com>
2023-10-09 20:02:36 -0300

    fix: auth context
    fix the auth context problem with signin function beeing not recognized

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -1,11 +1,25 @@
 import { createContext, useState } from 'react'
 
-export const AuthContext = createContext({})
+type User = {
+	auth: boolean
+}
+
+type AuthContextType = {
+	user: User,
+	signin: () => void,
+	signout: () => void,
+}
+
+export const AuthContext = createContext<AuthContextType>({
+	user: {auth: false},
+	signin: () => {},
+	signout: () => {},
+})
 
 const AuthProvider = ({
     children
 }: any) => {
-	const [user, setUser] = useState({ auth: false })
+	const [user, setUser] = useState<User>({ auth: false })
 
 	const signin = () => {
 		setUser({ auth: true })
