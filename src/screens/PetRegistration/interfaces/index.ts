3a841b1b9fc59c123be4266086fb7a2f99ddf6a4
Mateus Valério <mateusvalerio1@gmail.com>
2023-11-06 19:22:00 -0300

    feat: new  additions to interfaces

--- src/screens/PetRegistration/interfaces/index.ts
+++ src/screens/PetRegistration/interfaces/index.ts
@@ -1,56 +1,61 @@
+export interface PhotoComponentProps {
+	imageUri: string
+	onChangeData: (imageUri: string) => void
+}
 
 interface petHealth {
-    castrated: boolean,
-    dewormed: boolean,
-    diseases: string,
-    sick: boolean
-    vaccinated: boolean
+	castrated: boolean
+	dewormed: boolean
+	diseases: string
+	sick: boolean
+	vaccinated: boolean
 }
 
-interface financialPreferences {
-    food: boolean,
-    health: boolean,
-    toys: boolean,
-}
-
-interface sponsorshipPreferences {
-    financial: financialPreferences
-    sponsorshipTerm: boolean
-    visits: boolean
-}
-
-interface helpPreferences {
-    financial: boolean
-    food: boolean
-    medicine : [string]
-    toys: [string]
-}
-
-interface followUpVisitsPreferences {
-    oneMonth: boolean
-    sixMonths: boolean
-    threeMonths: boolean
+export interface followUpVisitsPreferences {
+	oneMonth: boolean
+	sixMonths: boolean
+	threeMonths: boolean
 }
 
 export interface adoptionPreferences {
-    adoptionTerm: boolean,
-    followUpVisits: followUpVisitsPreferences
-    housePhotos: boolean
-    testVisit: boolean
+	adoptionTerm: boolean
+	followUpVisits: followUpVisitsPreferences
+	housePhotos: boolean
+	testVisit: boolean
+}
+
+export interface TemperamentType {
+	Brincalhão: boolean
+	Tímido: boolean
+	Calmo: boolean
+	Guarda: boolean
+	Amoroso: boolean
+	Preguiçoso: boolean
+}
+
+export interface CommonData {
+	temperament: TemperamentType
+	specie: string
+	gender: string
+	size: string
+	age: string
+	vaccinated: boolean
+	dewormed: boolean
+	castrated: boolean
+	sick: boolean
+	diseases: string
 }
 
 export interface IRegisterPet {
-    about: string
-    age_range: string
-    name: string
-    petHealth: petHealth
-    photos: [string]
-    sex: string
-    size: string
-    species: string
-    temper: [string]
-    owner: string 
-    sponsorshipPreferences?: sponsorshipPreferences
-    helpPreferences?: helpPreferences
-    adoptionPreferences?: adoptionPreferences
+	about: string
+	age_range: string
+	name: string
+	petHealth: petHealth
+	photos: [string]
+	sex: string
+	size: string
+	species: string
+	temper: [string]
+	owner: string
+	adoptionPreferences: adoptionPreferences
 }
