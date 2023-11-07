660f06602c2384a411354e3de61b4cd6e5a0d0e8
Mateus Valério <mateusvalerio1@gmail.com>
2023-11-07 16:20:46 -0300

    feat: basic fetch data from a single pet

--- src/screens/Adopt/PetInfo/index.tsx
+++ src/screens/Adopt/PetInfo/index.tsx
@@ -1,16 +1,49 @@
-import { Text, View } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
+import { doc, getDoc } from 'firebase/firestore'
+import { useCallback, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import { PetInfoProps } from 'routes/types'
+import { PetData } from '..'
+import { db } from '../../../services/firebase'
 
 const PetInfo = ({ route }: PetInfoProps) => {
-	const pet = route.params.pet
+	const petParam = route.params.pet
+	const [petInfo, setPetInfo] = useState<PetData>({} as PetData)
+
+	// fetchData
+	useFocusEffect(
+		useCallback(() => {
+			fetchData()
+		}, [])
+	)
+
+	const fetchData = async () => {
+		const data = await getDoc(doc(db, 'pet', petParam.id))
+		const petData = { id: data.id, ...data.data() } as PetData
+		setPetInfo(petData)
+	}
+
 	return (
-		<View>
-			<Text>Foto do Anibal</Text>
-			<Text>Nome: {pet.name}</Text>
-			<Text>Sexo: {pet.gender}</Text>
-			<Text>Localidade: {pet.location}</Text>
+		<View style={styles.container}>
+			<Text>Foto do Animal</Text>
+			<Text>Nome: {petInfo.name}</Text>
+			<Text>Sexo: {petInfo.sex}</Text>
+			<Text>Porte: {petInfo.size}</Text>
+			<Text>Castrado: {petInfo.petHealth?.castrated}</Text>
+			<Text>Vermifugado: {petInfo.petHealth?.dewormed}</Text>
+			<Text>Doenças: {petInfo.petHealth?.diseases}</Text>
+			<Text>Temperamento: {petInfo.temper}</Text>
+			<Text>
+				Mais sobre {petInfo.name}: {petInfo.about}
+			</Text>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+})
 
 export default PetInfo
