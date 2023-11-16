4de21934a288e27092301c9f830049b836cd7280
Mateus Valério <mateusvalerio1@gmail.com>
2023-11-16 17:43:10 -0300

    fix: profile image infnite fetch

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,32 +1,40 @@
-import { useContext, useState } from 'react'
+import { getDownloadURL, ref } from 'firebase/storage'
+import { useContext, useEffect, useState } from 'react'
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import MainButton from '../../components/MainButton'
 import { AuthContext } from '../../context/Auth'
+import { storage } from '../../services/firebase'
 import ProfilePhoto from './components/ProfilePhoto'
 import ShowValue from './components/ShowValue'
-import { storage } from '../../services/firebase'
-import { ref, getDownloadURL } from "firebase/storage";
 
 const Profile = () => {
 	const { user } = useContext(AuthContext)
 	const [urlT, setUrlT] = useState({
 		loading: true,
-		source: {uri: ''}
+		source: { uri: '' },
 	})
 	const mockHistory = 'Adotou 1 gato'
 
-	getDownloadURL(ref(storage, `user/${user.user_uid}/profilePicture.png`)).then((x) => {
-		setUrlT({
-			loading: false,
-			source: {uri: x}
-		})
-	}).catch((error) => {
-		console.log(error)
-		setUrlT({
-			loading: false,
-			source: require('../../assets/images/default-pf.png')
-		})
-	})
+	useEffect(() => {
+		fetchProfileImage()
+	}, [])
+
+	const fetchProfileImage = () => {
+		getDownloadURL(ref(storage, `user/${user.user_uid}/profilePicture.png`))
+			.then((x) => {
+				setUrlT({
+					loading: false,
+					source: { uri: x },
+				})
+			})
+			.catch((error) => {
+				console.warn('profile: ', error)
+				setUrlT({
+					loading: false,
+					source: require('../../assets/images/default-pf.png'),
+				})
+			})
+	}
 
 	return (
 		<>
@@ -81,8 +89,8 @@ const styles = StyleSheet.create({
 		fontSize: 16,
 	},
 	buttonContainer: {
-		marginBottom: 20
-	}
+		marginBottom: 20,
+	},
 })
 
 export default Profile
