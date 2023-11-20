97ffa2fbdbb301c271d296d121bed9d4a805e59c
TokarskiThiago <thiago.tokarski2001@gmail.com>
2023-11-20 20:05:33 -0300

    fix: add await

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ async function registerForPushNotificationsAsync() {
 	let token = {} as Notifications.ExpoPushToken;
   
 	if (Platform.OS === 'android') {
-		Notifications.setNotificationChannelAsync('default', {
+		await Notifications.setNotificationChannelAsync('default', {
 			name: 'default',
 			importance: Notifications.AndroidImportance.MAX,
 			vibrationPattern: [0, 250, 250, 250],
