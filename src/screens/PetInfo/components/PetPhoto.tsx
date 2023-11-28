e4a19b9ff5254c6e16fe3f711d15ed3d61e76b26
Mateus Valério <mateusvalerio1@gmail.com>
2023-11-28 14:39:27 -0300

    feat: Carousel Component at PetInfo
    Criação do component Carousel para apresentar várias imagens

--- src/screens/PetInfo/components/PetPhoto.tsx
+++ src/screens/PetInfo/components/PetPhoto.tsx
@@ -1,23 +1,27 @@
-import { useEffect, useState } from 'react'
-import { ActivityIndicator, Image, StyleSheet, View } from 'react-native'
-import { fetchedImageUrl } from '../../../services/images'
+import { useEffect, useRef, useState } from 'react'
+import { ActivityIndicator, Dimensions, FlatList, Image, StyleSheet, View } from 'react-native'
+import { fetchedImagesUrl } from '../../../services/images'
 
 type Props = {
 	petId: string | undefined
 }
 
+const { width } = Dimensions.get('screen')
+
 const PetPhoto = ({ petId }: Props) => {
 	const defaultImage = require('../../../assets/images/default-pf.png')
 	const [loading, setLoading] = useState(false)
-	const [url, setUrl] = useState<string>('')
+	const [urls, setUrls] = useState<string[]>([''])
 
 	useEffect(() => {
 		if (!petId) return setLoading(true)
-		fetchedImageUrl({
-			storageUrl: `pet/${petId}/image_0.png`,
+		fetchedImagesUrl({
+			entityFolder: 'pet',
+			entityID: petId,
 			setLoading: (state: boolean) => setLoading(state),
-			setUrl: (url: string) => setUrl(url),
+			setUrls: (urls: string[]) => setUrls(urls),
 		})
+		console.log('urls: ', urls)
 	}, [petId])
 
 	return (
@@ -28,10 +32,10 @@ const PetPhoto = ({ petId }: Props) => {
 				</View>
 			) : (
 				<View style={{ flex: 1 }}>
-					{url === '' ? (
+					{urls[0] === '' ? (
 						<Image style={styles.image} source={defaultImage} />
 					) : (
-						<Image style={styles.image} source={{ uri: url }} />
+						<Carousel urls={urls} />
 					)}
 				</View>
 			)}
@@ -39,10 +43,49 @@ const PetPhoto = ({ petId }: Props) => {
 	)
 }
 
+const Carousel = ({ urls }: { urls: string[] }) => {
+	const [index, setIndex] = useState(0)
+	const handleOnViewableItemsChanged = useRef(({ viewableItems }: any) => {
+		console.log('viewableItems', viewableItems)
+		setIndex(viewableItems[0].index)
+	}).current
+
+	// const viewabilityConfig = useRef({
+	// 	itemVisiblePercenteThreshold: 50,
+	// }).current
+
+	return (
+		<>
+			<FlatList
+				data={urls}
+				renderItem={({ item }) => <Image style={styles.image} source={{ uri: item }} />}
+				horizontal
+				pagingEnabled
+				showsHorizontalScrollIndicator={false}
+				snapToAlignment='center'
+				onViewableItemsChanged={handleOnViewableItemsChanged}
+				// viewabilityConfig={viewabilityConfig}
+			/>
+			<Pagination data={urls} index={index} />
+		</>
+	)
+}
+
+const Pagination = ({ data, index }: { data: string[]; index: number }) => {
+	if (data.length < 2) return
+	return (
+		<View style={styles.dotContainer}>
+			{data.map((_, idx) => (
+				<View key={idx.toString()} style={[styles.dot, idx === index && styles.dotActive]} />
+			))}
+		</View>
+	)
+}
+
 const styles = StyleSheet.create({
 	container: {
 		height: '100%',
-		width: '100%',
+		width,
 	},
 	loadingContainer: {
 		height: '100%',
@@ -51,8 +94,27 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 	},
 	image: {
-		width: '100%',
+		width,
 		height: '100%',
+	},
+	dotContainer: {
+		position: 'absolute',
+		bottom: 15,
+		flexDirection: 'row',
+		gap: 12,
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	dot: {
+		width: 12,
+		height: 12,
+		borderRadius: 6,
+		borderWidth: 2,
+		borderColor: '#ccc',
+	},
+	dotActive: {
+		backgroundColor: '#ccc',
 	},
 })
 
