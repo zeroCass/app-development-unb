44d169612149f0ed84b988f0dabfc00c0bb84d3a
Mateus Valério <mateusvalerio1@gmail.com>
2023-11-28 17:01:28 -0300

    feat: sendPushNotification in Pet Adoption
    Alteração da função registerAdoptionRequest no component PetInfo e do service
    Agora, a aplicação manda a notificação e também grava no banco de dados caso nenhum erro tenha ocorrido

--- src/screens/PetInfo/services/index.ts
+++ src/screens/PetInfo/services/index.ts
@@ -1,32 +1,62 @@
-import { doc, getDoc } from "firebase/firestore";
-import { db } from '../../../services/firebase';
-import { UserData } from "../interfaces";
+import { addDoc, collection, doc, getDoc } from 'firebase/firestore'
+import { TUser } from '../../../context/Auth'
+import { db } from '../../../services/firebase'
+import { UserData } from '../interfaces'
 
-export const serviceNotifyPetOwner:any = async (owner: string) => {
-    
-    const data = await getDoc(doc(db, 'users', owner))
-    const userData = { id: data.id, ...data.data() } as UserData
+export type NotifyPetOwnerType = {
+	owner: string
+	myExpoToken: string
+	user: TUser
+	petName: string
+}
+
+export const serviceNotifyPetOwner = async (
+	owner: string,
+	myExpoToken: string,
+	user: TUser,
+	petName: string
+) => {
+	const data = await getDoc(doc(db, 'users', owner))
+	const userData = { id: data.id, ...data.data() } as UserData
 
 	try {
 		const message = {
-            to: userData.expoToken,
-            sound: 'default',
-            title: 'Adoção',
-            body: 'Uma pessoa quer adotar seu pet!',
-            data: { someData: 'goes here' },
-        };
-    
-        await fetch('https://exp.host/--/api/v2/push/send', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Accept-encoding': 'gzip, deflate',
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(message),
-        });
-		return { type: "success" }
+			to: userData.expoToken,
+			sound: 'default',
+			title: 'Adoção',
+			body: `${user.full_name} quer adotar seu pet!`,
+			data: { someData: `${user.full_name} deseja adotar seu pet de nome ${petName}` },
+		}
+
+		console.warn('message: ', message)
+
+		const res = await fetch('https://exp.host/--/api/v2/push/send', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Accept-encoding': 'gzip, deflate',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(message),
+		})
+
+		// Extract the ID from the response
+		const { data } = await res.json()
+		if (data.status === 'error') return { type: 'error', error: 'Falha ao enviar mensagem' }
+
+		// add notification to database
+		const notification = {
+			to: userData.expoToken,
+			from: myExpoToken,
+			title: message.title,
+			body: message.body,
+			data: message.data,
+			viewed: false,
+		}
+		await addDoc(collection(db, 'notifications'), notification)
+
+		return { type: 'success' }
 	} catch (erro) {
-		return { type: "error", error: String(erro) }
+		return { type: 'error', error: String(erro) }
 	}
 }
--- src/screens/PetRegistration/services/index.ts
+++ src/screens/PetRegistration/services/index.ts
@@ -1,25 +1,25 @@
-import { addDoc, collection } from "firebase/firestore";
-import { db, storage } from '../../../services/firebase';
+import { addDoc, collection } from 'firebase/firestore'
+import { ref, uploadBytes } from 'firebase/storage'
+import { db, storage } from '../../../services/firebase'
 import { IRegisterPet } from '../interfaces'
-import { ref, uploadBytes } from "firebase/storage";
 
 async function uploadPetImage(imageUri: string, petUid: string, storageFileName: string) {
-	const img = await fetch(imageUri);
-	const blob = await img.blob();
-	const petRef = ref(storage, `pet/${petUid}/${storageFileName}.png`);
+	const img = await fetch(imageUri)
+	const blob = await img.blob()
+	const petRef = ref(storage, `pet/${petUid}/${storageFileName}.png`)
 	try {
-		await uploadBytes(petRef, blob);
+		await uploadBytes(petRef, blob)
 	} catch (error) {
 		console.warn(error)
 	}
 }
 
-export const registerPet:any = async (petData: IRegisterPet, imageUri: string) => {
+export const registerPet: any = async (petData: IRegisterPet, imageUri: string) => {
 	try {
-		const pet = await addDoc(collection(db, "pet"), petData);
-		await uploadPetImage(imageUri, pet.id, petData.photos[0]);
-		return { type: "success" }
+		const pet = await addDoc(collection(db, 'pet'), petData)
+		await uploadPetImage(imageUri, pet.id, petData.photos[0])
+		return { type: 'success' }
 	} catch (erro) {
-		return { type: "error", error: String(erro) }
+		return { type: 'error', error: String(erro) }
 	}
 }
