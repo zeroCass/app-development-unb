5f9ff8846565c8681218fde9eb53ae053f2eb30b
Mateus Valério <mateusvalerio1@gmail.com>
2023-12-05 18:29:00 -0300

    update: user type in PetInfo interfaces

--- src/screens/PetInfo/interfaces/index.ts
+++ src/screens/PetInfo/interfaces/index.ts
@@ -1,12 +1,12 @@
 export interface UserData {
-    id: string;
-    address: string;
-    age: number;
-    city: string;
-    email: string;
-    expoToken: string;
-    full_name: string;
-    phone: string;
-    state: string;
-    username: string;
+	user_uid: string
+	address: string
+	age: number
+	city: string
+	email: string
+	expoToken: string
+	full_name: string
+	phone: string
+	state: string
+	username: string
 }
