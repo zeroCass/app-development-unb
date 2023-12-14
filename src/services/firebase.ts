f5bb6dbbee35009d18375ce6dda1192e7a16910a
Mateus Valério <mateusvalerio1@gmail.com>
2023-12-14 15:20:44 -0300

    feat: async storage persistence
    Os dados do usuário são armazenados localmente, fazendo login automaticamente. O expopushtoken também é atualizado automaticamente quando o usuário está logado ou não

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,34 +1,35 @@
+import {
+	EXPO_PUBLIC_API_KEY,
+	EXPO_PUBLIC_APP_ID,
+	EXPO_PUBLIC_AUTH_DOMAIN,
+	EXPO_PUBLIC_MEASUREMENT_ID,
+	EXPO_PUBLIC_MESSAGING_SENDER_ID,
+	EXPO_PUBLIC_PROJECT_ID,
+	EXPO_PUBLIC_STORAGE_BUCKET,
+} from '@env'
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth'
-import { getStorage } from "firebase/storage";
-import { 
-    EXPO_PUBLIC_API_KEY,
-    EXPO_PUBLIC_AUTH_DOMAIN,
-    EXPO_PUBLIC_PROJECT_ID,
-    EXPO_PUBLIC_STORAGE_BUCKET,
-    EXPO_PUBLIC_MESSAGING_SENDER_ID,
-    EXPO_PUBLIC_APP_ID,
-    EXPO_PUBLIC_MEASUREMENT_ID
-} from "@env"
+import { getAuth, getReactNativePersistence, initializeAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: EXPO_PUBLIC_API_KEY,
-    authDomain: EXPO_PUBLIC_AUTH_DOMAIN,
-    projectId: EXPO_PUBLIC_PROJECT_ID,
-    storageBucket: EXPO_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: EXPO_PUBLIC_MESSAGING_SENDER_ID,
-    appId: EXPO_PUBLIC_APP_ID,
-    measurementId: EXPO_PUBLIC_MEASUREMENT_ID,
+	apiKey: EXPO_PUBLIC_API_KEY,
+	authDomain: EXPO_PUBLIC_AUTH_DOMAIN,
+	projectId: EXPO_PUBLIC_PROJECT_ID,
+	storageBucket: EXPO_PUBLIC_STORAGE_BUCKET,
+	messagingSenderId: EXPO_PUBLIC_MESSAGING_SENDER_ID,
+	appId: EXPO_PUBLIC_APP_ID,
+	measurementId: EXPO_PUBLIC_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
+initializeAuth(app, {
+	persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+})
+
 const auth = getAuth(app)
 const db = getFirestore(app)
-const storage = getStorage();
+const storage = getStorage()
 
-export {
-    db,
-    auth,
-    storage
-}
+export { auth, db, storage }
